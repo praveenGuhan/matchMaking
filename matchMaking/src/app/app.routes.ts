d6a1c930966d243954b179d6956e23e6dfcd5ee3
praveen <praveenleo2311@gmail.com>
2024-04-13 08:38:15 +0530

    feat: angular material package installed.

--- matchMaking/src/app/app.routes.ts
+++ matchMaking/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { InputComponent } from './component/input/input.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'input',component:InputComponent}
+];
